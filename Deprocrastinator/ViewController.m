d5a7be2b2f8200f2fc04a0849930e5fa79503642
Adam Cooper <acooper@rallydev.com>
2014-10-10 19:42:46 -0500

    Finished Challenge

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -11,13 +11,17 @@
 @interface ViewController () <UITableViewDelegate, UITableViewDataSource, UIGestureRecognizerDelegate, UIAlertViewDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *viewControllerTextField;
 @property NSMutableArray *errandsArray;
+
 @property (weak, nonatomic) IBOutlet UITableView *tableViewErrands;
-@property NSIndexPath *lastIndexPath;
+
 @property NSMutableArray *checkedIndexPaths;
+@property NSMutableArray *colorIndexPaths;
+
 @property (weak, nonatomic) IBOutlet UIButton *editButton;
-@property NSArray *colorsArray;
+
 @property CGPoint originalCenter;
 @property NSIndexPath *alertIndexPath;
+
 
 
 
@@ -27,37 +31,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.errandsArray = [NSMutableArray arrayWithObjects:@"Help old People",
-                                                    @"Help people",
-                                                    @"Help Don",
-                                                    @"Help people",
-                                                    @"Help Don",
-                                                    @"Help people",
-                                                    @"Help Don",
-                                                    @"Help people",
-                                                    @"Help Don",
-                                                    @"Help people",
-                                                    @"Help Don",
-                                                    @"Help people",
-                                                    @"Help Don",
-                                                    @"Help people",
-                                                    @"Help Don",
-                                                    @"Help Humanity", nil];
-
+    
+    self.errandsArray = [[NSMutableArray alloc] initWithObjects:@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello",@"Hello", nil];
+    
+    self.colorIndexPaths = [[NSMutableArray alloc] init];
     self.checkedIndexPaths = [[NSMutableArray alloc]init];
     for (int i = 0; i < self.errandsArray.count; i++) {
         [self.checkedIndexPaths addObject:[NSNumber numberWithBool:NO]];
+        [self.colorIndexPaths addObject:@"White"];
     }
-    
-    self.colorsArray = [NSMutableArray arrayWithObjects:[UIColor redColor], [UIColor yellowColor], [UIColor greenColor], nil];
-    
-
     
 }
 
 - (IBAction)onAddButtonPressed:(id)sender {
     
-    [self.errandsArray addObject:self.viewControllerTextField.text];
+    
     [self.checkedIndexPaths addObject:[NSNumber numberWithBool:NO]];
     self.viewControllerTextField.text = @"";
     [self.viewControllerTextField resignFirstResponder];
@@ -65,7 +53,6 @@
     
 }
 - (IBAction)onEditButtonPressed:(UIButton *)sender {
-
     if (self.tableViewErrands.editing) {
         [self.editButton setTitle:@"Edit" forState:UIControlStateNormal];
         [self.tableViewErrands setEditing:NO animated:YES];
@@ -74,9 +61,6 @@
         [self.tableViewErrands setEditing:YES animated:YES];
     }
 }
-
-
-
 
 
 -(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -91,7 +75,7 @@
                                               otherButtonTitles:@"YES", nil];
         [alert show];
     }
-
+    
 }
 
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
@@ -104,94 +88,94 @@
 
 
 - (IBAction)prioritySwipeRight:(UISwipeGestureRecognizer*)swipegesture{
-        //Get location of the swipe
-        CGPoint location = [swipegesture locationInView:self.tableViewErrands];
-        
-        //Get the corresponding index path within the table view
-        NSIndexPath *indexPath = [self.tableViewErrands indexPathForRowAtPoint:location];
-        
-        //Check if index path is valid
-        if(indexPath)
-        {
-            //Get the cell out of the table view
-            UITableViewCell *cell = [self.tableViewErrands cellForRowAtIndexPath:indexPath];
-            
-            //Update the cell or model
-            if (cell.tag == 0) {
-                cell.backgroundColor = [UIColor greenColor];
-                cell.tag++;
-            } else if(cell.tag == 1){
-                cell.backgroundColor = [UIColor yellowColor];
-                cell.tag++;
-            } else if(cell.tag == 2){
-                cell.backgroundColor = [UIColor redColor];
-                cell.tag++;
-            } else if(cell.tag == 3){
-                cell.backgroundColor = [UIColor whiteColor];
-                cell.tag = 0;
-            }
+    //Get location of the swipe
+    CGPoint location = [swipegesture locationInView:self.tableViewErrands];
+    
+    //Get the corresponding index path within the table view
+    NSIndexPath *indexPath = [self.tableViewErrands indexPathForRowAtPoint:location];
+    
+    NSInteger cool = [[self.colorIndexPaths objectAtIndex:indexPath.row]integerValue];
+    cool++;
+    
+    
+    
+    if (indexPath) {
+        //Update the cell or model
+        UITableViewCell *cell = [self.tableViewErrands cellForRowAtIndexPath:indexPath];
+        if (cell.tag == 0) {
+            [self.colorIndexPaths replaceObjectAtIndex:indexPath.row withObject:@"Green"];
+            cell.tag++;
+        } else if(cell.tag == 1){
+            [self.colorIndexPaths replaceObjectAtIndex:indexPath.row withObject:@"Yellow"];
+            cell.tag++;
+        } else if(cell.tag == 2){
+            [self.colorIndexPaths replaceObjectAtIndex:indexPath.row withObject:@"Red"];
+            cell.tag++;
+            NSLog(@"%@", [self.colorIndexPaths objectAtIndex:indexPath.row]);
+        } else if(cell.tag == 3){
+            [self.colorIndexPaths replaceObjectAtIndex:indexPath.row withObject:@"White"];
+            cell.tag = 0;
         }
+
+    }
+    
+    [self.tableViewErrands reloadData];
 }
 
 
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.errandsArray.count;
-    
 }
 
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"deprocrastinatorCell" forIndexPath:indexPath];
-    cell.textLabel.text = [self.errandsArray objectAtIndex:indexPath.row];
+    NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
-    if ([indexPath compare:self.lastIndexPath] == NSOrderedSame)
+    cell.textLabel.text = [self.errandsArray objectAtIndex:indexPath.row];
+    [cell setAccessoryType:UITableViewCellAccessoryNone];
+    
+    
+    BOOL shouldBeChecked = [[self.checkedIndexPaths objectAtIndex:indexPath.row] boolValue];
+    
+    if (shouldBeChecked)
     {
         cell.accessoryType = UITableViewCellAccessoryCheckmark;
-        [self.checkedIndexPaths replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:YES]];
-
     }
-    else
-    {
-        cell.accessoryType = UITableViewCellAccessoryNone;
-        [self.checkedIndexPaths replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:NO]];
+    
+    if (self.colorIndexPaths){
         
+        if ([[self.colorIndexPaths objectAtIndex:indexPath.row] isEqualToString:@"White"]) {
+            cell.backgroundColor = [UIColor whiteColor];
+        } else if ([[self.colorIndexPaths objectAtIndex:indexPath.row] isEqualToString:@"Green"]){
+            cell.backgroundColor = [UIColor greenColor];
+        } else if ([[self.colorIndexPaths objectAtIndex:indexPath.row] isEqualToString:@"Yellow"]){
+            cell.backgroundColor = [UIColor yellowColor];
+        } else if ([[self.colorIndexPaths objectAtIndex:indexPath.row] isEqualToString:@"Red"]){
+            cell.backgroundColor = [UIColor redColor];
+        }
     }
+
+
+    
     return cell;
-    
 }
 
-
--(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
-
     
-    
-}
-
--(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath{
-    return YES;
-}
-
-
-
-
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+
+    BOOL currentValue = [[self.checkedIndexPaths objectAtIndex:indexPath.row] boolValue];
+    BOOL updatedValue = !currentValue;
     
-    if (cell.accessoryType == UITableViewCellAccessoryNone){
-        //This sets the array
-        [self.checkedIndexPaths replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:YES]];
-        
-    } else{
-        //This sets the array
-        [self.checkedIndexPaths replaceObjectAtIndex:indexPath.row withObject:[NSNumber numberWithBool:NO]];
-        
-    }
-    [self.tableViewErrands reloadData];
+    self.checkedIndexPaths[indexPath.row] = [NSNumber numberWithBool:updatedValue];
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [tableView reloadData];
+
 }
 
-
-
+    
 @end
